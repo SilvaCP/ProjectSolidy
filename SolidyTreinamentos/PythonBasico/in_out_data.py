719bfb010afb07db49a5af9fc986b2e4e0b3c48f
Carlos Pereira <silvaotl@outlook.com>
2025-12-15 00:22:36 -0300

    Conclusão da aula 2.

---
+++ SolidyTreinamentos/PythonBasico/in_out_data.py
@@ -0,0 +1,38 @@
+'''
+nome = input("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+altura = input("Informe a sua altura:")
+
+print(f'{nome} você tem {idade} anos de idade e sua altura é {altura}:')
+
+num1 = 10
+num2 = 2
+num3 = 5.5
+resultado = 16 ** (1/2)
+print(resultado)
+'''
+
+# EXERCÍCIO: Exercise:
+"""
+Faça um formulário que pergunte o nome, cpf, endereço, idade, altura e
+telefone e imprima isso em um relatório organizado.
+
+Make a form that asks for name, social security number, address, age, height and 
+phone and print this in an organized report.
+"""
+print(14 * '-')
+print('FORMULÁRIO:')
+name = input('Enter your name: ')
+cpf = int(input('Enter your social security number: '))
+eddress = input('Enter your eddress: ')
+age = int(input('Enter your age: '))
+height = float(input('Enter your height: '))
+phone = int(input('Enter your phone number: '))
+print(f'name: {name}, cpf: {cpf}, eddress: {eddress}, age: {age}, height: {height}, phone: {phone}')
+print(14 * '-')
+
+
+
+
+
+
